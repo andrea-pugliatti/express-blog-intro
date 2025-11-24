79449598c1c6cdaf7fd2ff58e6a28b7ca53f0b29
andrea-pugliatti <andrea.pugliatti@outlook.it>
2025-11-24 11:44:31 +0100

    Add the index route and add start and dev scripts.

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+const PORT = 3000;
+
+app.listen(PORT, () => {
+	console.log(`Listening on http://localhost:${PORT}`);
+});
+
+app.get("/", (req, res) => {
+	res.send("Server del mio blog");
+	console.log(req.query);
+});
