cf6df70d238f6fb4f015b1d6fb494181da4b27f1
andrea-pugliatti <andrea.pugliatti@outlook.it>
2025-11-24 11:48:51 +0100

    Add posts array

--- index.js
+++ index.js
@@ -3,6 +3,44 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
+const posts = [
+	{
+		id: 1,
+		title: "Il mio primo giorno a scuola",
+		content: "Lorem",
+		img: "/scuola.jpg",
+		tags: ["scuola", "security policy"],
+	},
+	{
+		id: 2,
+		title: "Il mio secondo giorno a scuola",
+		content: "Lorem",
+		img: "/scuola.jpg",
+		tags: ["scuola", "assistenza clienti"],
+	},
+	{
+		id: 3,
+		title: "Il mio terzo giorno a scuola",
+		content: "Lorem",
+		img: "/scuola.jpg",
+		tags: ["scuola", "management"],
+	},
+	{
+		id: 4,
+		title: "Il mio quarto giorno a scuola",
+		content: "Lorem",
+		img: "/scuola.jpg",
+		tags: ["scuola", "macroeconomics"],
+	},
+	{
+		id: 5,
+		title: "Il mio quinto giorno a scuola",
+		content: "Lorem",
+		img: "/scuola.jpg",
+		tags: ["scuola", "physics"],
+	},
+];
+
 app.listen(PORT, () => {
 	console.log(`Listening on http://localhost:${PORT}`);
 });
