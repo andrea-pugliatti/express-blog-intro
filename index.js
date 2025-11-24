bbc684cc31b61813f335c4fefd23f6039711b2ed
andrea-pugliatti <andrea.pugliatti@outlook.it>
2025-11-24 11:54:12 +0100

    Add static assets in public and make them available on the server

--- index.js
+++ index.js
@@ -8,35 +8,35 @@ const posts = [
 		id: 1,
 		title: "Il mio primo giorno a scuola",
 		content: "Lorem",
-		img: "/scuola.jpg",
+		img: "/images/ciambellone.jpeg",
 		tags: ["scuola", "security policy"],
 	},
 	{
 		id: 2,
 		title: "Il mio secondo giorno a scuola",
 		content: "Lorem",
-		img: "/scuola.jpg",
+		img: "/images/cracker_barbabietola.jpeg",
 		tags: ["scuola", "assistenza clienti"],
 	},
 	{
 		id: 3,
 		title: "Il mio terzo giorno a scuola",
 		content: "Lorem",
-		img: "/scuola.jpg",
+		img: "/images/pane_fritto_dolce.jpeg",
 		tags: ["scuola", "management"],
 	},
 	{
 		id: 4,
 		title: "Il mio quarto giorno a scuola",
 		content: "Lorem",
-		img: "/scuola.jpg",
+		img: "/images/pasta_barbabietola.jpeg",
 		tags: ["scuola", "macroeconomics"],
 	},
 	{
 		id: 5,
 		title: "Il mio quinto giorno a scuola",
 		content: "Lorem",
-		img: "/scuola.jpg",
+		img: "/images/torta_paesana.jpeg",
 		tags: ["scuola", "physics"],
 	},
 ];
@@ -53,3 +53,5 @@ app.get("/", (req, res) => {
 app.get("/bacheca", (req, res) => {
 	res.json(posts);
 });
+
+app.use(express.static("public"));
